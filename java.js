5444e9de876e9364a54a85330f2431c9cabcf67e
Brian Stevenson <81999388+BStevens1987@users.noreply.github.com>
2024-02-04 23:19:49 -0800

    Create validation for phone number and message for password validation

---
+++ java.js
@@ -0,0 +1,10 @@
+var passConfirm = function () {
+  if (document.getElementById("pass").value == document.getElementById("cpass").value) {
+    document.getElementById("message").style.color = "Green";
+    document.getElementById("message").innerHTML = "Passwords match!";
+  }
+  else {
+    document.getElementById("message").style.color = "Red";
+    document.getElementById("message").innerHTML = "Passwords do NOT match!";
+  }
+}
